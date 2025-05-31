4488b97a298ebe9ee027b297d142ca5a112aa501
NguyenHoangAnh411 <131466310+NguyenHoangAnh411@users.noreply.github.com>
2025-05-31 16:14:19 +0700

    Fix bug build

---
+++ client/src/pages/redeem-code.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RedeemCode: React.FC = () => {
+  return (
+    <div>
+      <h1>Redeem Code Page</h1>
+      {/* Add your redeem code form or content here */}
+    </div>
+  );
+};
+
+export default RedeemCode;
